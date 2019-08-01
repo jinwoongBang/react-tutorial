b30447d974713dac6595937eda986a650967aa53
jinwoongBang <dlopo123@gmail.com>
2019-08-01 18:21:37 +0900

    input 과 상태관리

---
+++ react-input-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PhoneForm from './components/PhoneForm';
+
+export default class App extends Component {
+  handleCreate = (data) => {
+    console.log(data);
+  }
+
+  render() {
+      return (
+          <div>
+              <PhoneForm onCreate={this.handleCreate}/>
+          </div>
+      );
+  }
+}
