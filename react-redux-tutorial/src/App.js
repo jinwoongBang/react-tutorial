6ad3f9ed27b54bc3d78eba501c8d6f56b044037d
jinwoongBang <dlopo123@gmail.com>
2019-08-02 14:22:00 +0900

    react redux 학습 업로드

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,35 +1,26 @@
-import React, { Component } from "react";
-import MyName from "./components/MyName";
-import Counter1 from "./components/Counter_setState";
-import Counter2 from "./components/Counter_life_cycle";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      test: "test",
-      index: 0
-    };
-  }
-
-  refreshBtn = () => {
-      this.setState(({index}) => ({
-          index: index + 1
-      }))
-    this.setState(({test, index}) => ({
-        test: test + index
-    }))
-  };
-
-  render() {
-    // return <MyName name="test" />;
-    return (
-      <div className="border">
-        <Counter2 test={this.state.test} />
-        <button onClick={this.refreshBtn}>refresh</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
