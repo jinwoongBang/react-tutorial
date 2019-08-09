81ce7687a2c4a4c6819debb8fd6fb3a80e1e1b20
jinwoongBang <dlopo123@gmail.com>
2019-08-09 18:17:51 +0900

    redux thunk

--- react-thunk-tutorial/src/App.js
+++ react-thunk-tutorial/src/App.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './components/modules/counter';
+import axios from 'axios';
 
 class App extends Component {
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+             .then(response => console.log(response.data));
+  }
+
   render() {
     const { CounterActions, number } = this.props;
 
     return (
       <div>
         <h1>{number}</h1>
-        <button onClick={CounterActions.increment}>+</button>
-        <button onClick={CounterActions.decrement}>-</button>
+        {/* [1] Redux-Thunk 미 적용 */}
+        {/* <button onClick={CounterActions.increment}>+</button>
+        <button onClick={CounterActions.decrement}>-</button> */}
+
+        {/* [2] Redux-Thunk 적용 */}
+        <button onClick={CounterActions.incrementAsync}>+</button>
+        <button onClick={CounterActions.decrementAsync}>-</button>
       </div>
     );
   }
--- react-thunk-tutorial/src/store.js
+++ react-thunk-tutorial/src/store.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './components/modules';
 import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
-const store = createStore(modules, applyMiddleware(loggerMiddleware));
+// const store = createStore(modules, applyMiddleware(loggerMiddleware));
+
+const logger = createLogger();
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk));
 
 export default store;
